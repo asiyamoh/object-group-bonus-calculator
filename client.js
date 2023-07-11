8bfaa09531cc439970e2d1df34a90161f7e3ae04
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2023-07-11 15:04:11 -0500

    made change to

--- client.js
+++ client.js
@@ -50,8 +50,55 @@ console.log('array of employee data: ',  employees );
 //
 function calculateIndividualEmployeeBonus( employee ) {  
   // your logic here
+  console.log(employee);
+  //create a new empty object
+  let bonusEmployee ={};
+for(let eachEmployee of employees){
+  console.log(eachEmployee.name);
+  bonusEmployee.name = eachEmployee.name;
+  console.log(eachEmployee.reviewRating);
+  if (eachEmployee.reviewRating < 2) {
+    bonusEmployee.bonus = 0;
+    console.log(bonusEmployee.bonus);
+  }
+  else if (eachEmployee.reviewRating == 3) {
+    bonusEmployee.bonus = 0.04 * eachEmployee.annualSalary;
+    console.log(bonusEmployee.bonus);
+  }
+  else if (eachEmployee.reviewRating == 4) {
+    bonusEmployee.bonus = 0.06 * eachEmployee.annualSalary;
+    console.log(bonusEmployee.bonus);
+  }
+  else if (eachEmployee.reviewRating == 5) {
+    bonusEmployee.bonus = 0.10 * eachEmployee.annualSalary;
+    console.log(bonusEmployee.bonus);
+  }
+  let vetEmployee = 0;
+  if(eachEmployee.employeeNumber.length === 4) {
+    vetEmployee = 0.05 * eachEmployee.annualSalary;
+    bonusEmployee.bonus += vetEmployee;
+    console.log(bonusEmployee.bonus);
+
+  }
+  let tooMuchMoney = 0;
+  if(eachEmployee.annualSalary > 65000){
+    tooMuchMoney = 0.01 * eachEmployee.annualSalary;
+    eachEmployee.bonus = tooMuchMoney - eachEmployee.annualSalary;
+    console.log(bonusEmployee.bonus);
+  }
+  console.log(bonusEmployee);
+}
   
   
   // return new object with bonus results
 
 }
+
+// employee.filter(bonusEmployee) => {
+//   return
+// }
+
+calculateIndividualEmployeeBonus(employees);
+// calculateIndividualEmployeeBonus(employee.reviewRating);
+// console.log(bonusEmployee);
+
